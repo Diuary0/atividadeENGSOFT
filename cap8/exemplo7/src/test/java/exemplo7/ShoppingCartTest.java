032b7654b5bcd46ea702e8c8eece6f32659efa41
Marco Tulio Valente <mtvalente@gmail.com>
2025-09-27 18:08:12 -0300

    cap8

--- cap8/exemplo7/src/test/java/exemplo7/ShoppingCartTest.java
+++ cap8/exemplo7/src/test/java/exemplo7/ShoppingCartTest.java
@@ -11,14 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartTest {
   
-    @Test
-    public void testAddGetTotal() {
-      Book b1 = new Book("book1", 10, "1");
-      Book b2 = new Book("book2", 20, "2");
-      ShoppingCart cart = new ShoppingCart();
-      cart.add(b1);
-      cart.add(b2);
-      assertEquals(30.0, cart.getTotal(), 0.01);
-    }
+  @Test
+  public void testAddGetTotal() {
+    Book b1 = new Book("book1", 10, "1");
+    Book b2 = new Book("book2", 20, "2");
+    ShoppingCart cart = new ShoppingCart();
+    cart.add(b1);
+    cart.add(b2);
+    assertEquals(30.0, cart.getTotal(), 0.01);
+  }
 
 }
